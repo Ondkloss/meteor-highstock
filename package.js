02fe9104ffc5e9c5564b2749f322bd94e47270ba
Halvor Strand <ondkloss@gmail.com>
2015-05-12 21:00:58 +0200

    Added libs and package.js

---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  name: 'ondkloss:highstock',
+  summary: 'Wrapper for Highstock with some additional modules',
+  version: '0.0.1',
+  git: 'https://github.com/Ondkloss/meteor-highstock.git'
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.1.0.2');
+    api.use('jquery');
+
+    api.addFiles([
+    	// Core
+    	'lib/highstock.js',
+    	// Extra types
+    	'lib/highcharts-more.js',
+    	'lib/highcharts-solide-gauge.js',
+    ], 'client');
+});
